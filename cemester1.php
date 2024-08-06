064166b2ff4deddac9dd1ca4f718bc9c6292b770
Ajaykanel <vikashkanelbhai@gmail.com>
2024-08-07 01:10:27 +0530

    Sheet1

---
+++ cemester1.php
@@ -0,0 +1,265 @@
+<html>
+<head>
+<style>
+body{text-align:center; font-weight:bold;}
+.logo{height:100px; position:absolute; margin-left:-480px; margin-top:50px;}
+h1{color:red;}
+.col{color:red;}
+table{font-weight:bold; margin-bottom:20px;}
+pre{text-align:left;}
+.date{text-align:left; display:flex;}
+.date p{margin-right:200px; font-weight:bold;}
+.end{text-align:left;color:red;  font-weight:bold; }
+#left{margin-left:50px; color:black; font-weight:bold;}
+span{margin-left:800px;  color:red;}
+</style>
+
+</head>
+<body>
+<form >
+
+<img src="https://rb.gy/j0w6a3" class="logo">
+<h1>MAHKANLAL CHATURVEDI NATIONAL UNIVERSITY OF <br> JOURNALISM AND COMMUNICATION BHOPAL </h1>
+<P class="col">ESTABLISHED BY MADHYA PRADESH VIDHANSABHA NO.15 OF 1990 </P>
+<h2 class="col">Grade Card </h2>
+<p>BACHELOR OF COMPUTER APPLICATION (B.C.A.)SEMESTER-1(CBCS) </p>
+<P>DEV.2022-JAN-2023 </P>
+<pre>
+
+	Enrollment No. : 
+	Roll No.       :
+	Name           :
+	Father's Name  :
+	Mother's Name  :
+</pre>
+<table border="1" width="100%" cellspacing="0">
+	<tr>
+		<td rowspan="3">Course Code& Name</td>
+		<td colspan="8">Scheme of Marks</td>
+		<td colspan="4" rowspan="2" > Marks Obtained</td>
+		<td colspan="3" rowspan="2">Grades Obtained</td>
+			<tr>
+				<td colspan="3">Min.Pass Marks </td>
+				<td colspan="3">Max.Marks</td>
+				<td colspan="2">Out of Total</td>
+			</tr>
+			<tr>
+				<td>Th.</td>
+				<td>Pr.</td>
+				<td>Int.</td>
+				<td>Th.</td>
+				<td>Pr.</td>
+				<td>Int.</td>
+				<td>Marks</td>
+				<td>Cerdits<br>C</td>
+				
+				<td>Th.</td>
+				<td>Pr.</td>
+				<td>Int.</td>
+				<td>Total.</td>
+				<td>Grade<br>Letter</td>
+				<td>Grade<br> Points<br>G</td>
+				<td>Cerdits <br>Points<br>C X G</td>
+			</tr>
+	</tr>
+<tr>
+	<td>PC Packages</td>
+	<td>032</td>
+	<td></td>
+	<td>008</td>
+	<td>080</td>
+	<td></td>
+	<td>020</td>
+	<td>100</td>
+	<td>6.00</td>
+	<td>054</td>
+	<td>---</td>
+	<td>018</td>
+	<td>072</td>
+	<td>A</td>
+	<td>8.00</td>
+	<td>48.00</td>
+
+</tr>
+<tr>
+	<td>Programming With C and C++ </td>
+	<td>032</td>
+	<td>---</td>
+	<td>008</td>
+	<td>080</td>
+	<td>---</td>
+	<td>020</td>
+	<td>100</td>
+	<td>6.00</td>
+	<td>054</td>
+	<td>---</td>
+	<td>018</td>
+	<td>072</td>
+	<td>A</td>
+	<td>8.00</td>
+	<td>48.00</td>
+
+</tr>
+<tr>	
+	<td>Communication</td>
+	<td>032</td>
+	<td>---</td>
+	<td>008</td>
+	<td>080</td>
+	<td>---</td>
+	<td>020</td>
+	<td>100</td>
+	<td>6.00</td>
+	<td>054</td>
+	<td>---</td>
+	<td>018</td>
+	<td>072</td>
+	<td>A</td>
+	<td>8.00</td>
+	<td>48.00</td>
+
+</tr>
+<tr>	
+	<td>Funtamentals of Computer & Information Technology</td>
+	<td>032</td>
+	<td>---</td>
+	<td>008</td>
+	<td>080</td>
+	<td>---</td>
+	<td>020</td>
+	<td>100</td>
+	<td>6.00</td>
+	<td>054</td>
+	<td>---</td>
+	<td>018</td>
+	<td>072</td>
+	<td>A</td>
+	<td>8.00</td>
+	<td>48.00</td>
+
+
+</tr>
+<tr>
+	<td>Coputer Lab-1</td>
+	<td>032</td>
+	<td>---</td>
+	<td>008</td>
+	<td>080</td>
+	<td>---</td>
+	<td>020</td>
+	<td>100</td>
+	<td>6.00</td>
+	<td>054</td>
+	<td>---</td>
+	<td>018</td>
+	<td>072</td>
+	<td>A</td>
+	<td>8.00</td>
+	<td>48.00</td>
+
+</tr>
+<tr>
+	<td colspan="7" >Semester Grand Total</td>
+	<td>MM</td>
+	<td>MC</td>
+	<td colspan="3"></td>
+	<td>TM</td>
+	<td colspan="2"></td>
+	<td>TC</td>
+</tr>
+<tr>
+	<td colspan="16">SGPA(Current Semester)=TC/MC 7.10(out of 10)</td>
+</tr>
+
+
+</table>
+<table border="1" width="100%">
+	<tr> 
+		<td>Semester</td>
+		<td>FIRST</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+	</tr>
+	<tr> 
+		<td>Session</td>
+		<td>DEC 2022</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+	</tr>
+	<tr> 
+		<td>Total Marks Obtained(TM)</td>
+		<td>323</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+	</tr>
+	<tr> 
+		<td>Maximum Marks (MM)</td>
+		<td>500</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+	</tr>
+	<tr> 
+		<td>SGPA</td>
+		<td>7.10</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+	</tr>
+	<tr> 
+		<td>Result</td>
+		<td>PASS</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+		<td>****</td>
+	</tr>
+	<tr>
+		<td>Division : ****</td>
+		<td colspan="3">Grand Total : ****</td>
+		<td colspan="3"> Percentage: ****</td>
+		<td colspan="2">CGPA: ****</td>
+	</tr>
+</table>
+<div class="date">
+	<p>26-APR-2023</p>
+	<p>SC5552EC177</p>
+</div>
+
+<div class="end">
+<p id="left">Result Declaration Date:15-Mar-2023</p>
+<p>For guide lines please refer overieaf</p>
+<p>(SGPA-Semester Grade Point Average.CGPA-Cumulative Grade Point Average,TM-Total Marks Obtained</p>
+<p>MM-Maximum Marks.MC-Maximum Credits.TC-Total Credit Point Obtained. Th.Theory.Pr-Practical Int-internet) </p>
+<span>Controller of  Examination</span>
+</div>
+
+</form>
+</body>
+</html>
